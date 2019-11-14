7ece6668e3a405d741d52ee29ebcc21a64241643
Tojil <sergio@bellcantoali.com>
2019-11-15 00:08:19 +0100

    Initial Commit

---
+++ fightingGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fightingGame
+//
+//  Created by SCS on 15/11/2019.
+//  Copyright © 2019 Sergio Canto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
